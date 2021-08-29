0f6dc14ed7c3c768a7dfe514b26cd13f89768c3d
Saurabh Kumar <sauravgautam884@gmail.com>
2021-08-29 19:55:56 +0530

    add react-simple-keyboard

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react";
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
 function Main() {
 	const [timer, setTimer] = useState(5);
 	const [timeElapsed, setTimeElapsed] = useState(0);
+    let keyboardRef
 
 	useEffect(() => {
 		let stopTimer;
@@ -38,6 +41,33 @@ function Main() {
 						? timeElapsed % 60
 						: "0" + (timeElapsed % 60)}
 				</h2>
+                <Keyboard
+                            ref={r => keyboardRef = r}
+                            layout={{
+                                'default': [
+                                    '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+                                    '{tab} q w e r t y u i o p [ ] \\',
+                                    '{lock} a s d f g h j k l ; \' {enter}',
+                                    '{shift} z x c v b n m , . / {shift}',
+                                    '{space}'
+                                ],
+                                'shift': [
+                                    '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+                                    '{tab} Q W E R T Y U I O P { } |',
+                                    '{lock} A S D F G H J K L : " {enter}',
+                                    '{shift} Z X C V B N M < > ? {shift}',
+                                    '{space}'
+                                ]
+                            }}
+                            display={{
+                                '{bksp}': 'backspace',
+                                '{enter}': 'enter',
+                                '{tab}': 'tab',
+                                '{lock}': 'caps lock',
+                                '{shift}': 'shift',
+                                '{space}': ' ',
+                            }}
+                        />
 			</div>
 		</div>
 	);
